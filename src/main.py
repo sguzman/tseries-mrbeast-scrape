bcd287e391645b887a9d866456eb6e143e3ba7f8
Salvador Guzman <guzmansalv@gmail.com>
2024-05-14 09:26:25 -0700

    chore: Set up logging with timestamp and level

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
+import atexit
+import logging
 import requests
 import shelve
 
+# Set up logging with timestamp and level
+logging.basicConfig(
+    # Put the timestamp at the beginning of each log message
+    # Put level next
+    format="%(levelname)s: %(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG,
+)
+
 # store data in a file
 cache = shelve.open(".cache")
+
+
+def close_cache():
+    logging.info("Closing cache")
+    cache.close()
+
+
+atexit.register(close_cache)
 
 
 # Get HTTP response from the API or from the cache
