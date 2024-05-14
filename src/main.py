3b0f3c4434864b09d92c68e8ec3fb140a347d53a
Salvador Guzman <guzmansalv@gmail.com>
2024-05-14 09:44:24 -0700

    chore: Add web scraping functionality to retrieve YouTube video views

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import atexit
 import logging
 import requests
 import shelve
+from bs4 import BeautifulSoup
 
 # Set up logging with timestamp and level
 logging.basicConfig(
@@ -15,6 +16,8 @@ logging.basicConfig(
 # store data in a file
 cache = shelve.open(".cache")
 
+url = "https://socialblade.com/youtube/compare/t-series/mrbeast"
+
 
 def close_cache():
     logging.info("Closing cache")
@@ -24,20 +27,50 @@ def close_cache():
 atexit.register(close_cache)
 
 
+# Generate headers for the request
+def generate_headers():
+    return {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+    }
+
+
 # Get HTTP response from the API or from the cache
 def get_or_cache(url):
     if url in cache:
         return cache[url]
-    response = requests.get(url)
-    cache[url] = response.json()
+    response = requests.get(url, headers=generate_headers())
+    cache[url] = response.text
     return cache[url]
 
 
 def main():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    data = get_or_cache(url)
-    for post in data:
-        print(post["title"])
+    logging.info("Starting main")
+    data: str = get_or_cache(url)
+    data: BeautifulSoup = BeautifulSoup(data, "html.parser")
+
+    # Find by id
+    data = data.find("div", {"id": "videoviewsYTDYGraph"})
+
+    # Get script tag inside the div
+    data = data.find("script")
+
+    # Get the text inside the script tag
+    data = data.text
+
+    # Extract everything after the first occurrence of "Date,T-Series,MrBeast"
+    data = data.split("Date,T-Series,MrBeast")[1]
+
+    # And everything before the first occurrence of "", {"
+    data = data.split(", {")[0]
+
+    # S
+    logging.info(data)
+    logging.info("Ending main")
 
 
 if __name__ == "__main__":
